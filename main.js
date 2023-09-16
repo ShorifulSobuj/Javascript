03e6c6e62c872de33d44e429cff174a84c48b68f
Shorifu Sobuj <sobujaiom126@gmail.com>
2023-09-16 21:42:42 +0600

    1. problem solve

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 1 Solve the problem with Javascript  to find the maximum between two numbers.
+
 function findMaximum(num1, num2) {
     if (num1 > num2) {
         return num1;
@@ -11,18 +13,3 @@ const num2 = 100;
 const maximum = findMaximum(num1, num2);
 console.log("The maximum between two numbers: " + maximum)
 
-function findMaximum(num1, num2, num3) {
-    if (num1 >= num2 && num1 >= num3) {
-        return num1;
-    } else if (num2 >= num2 && num2 >= num3) {
-        return num2;
-    } else {
-        return num3;
-    }
-}
-
-const number1 = 23;
-const number2 = 34;
-const number3 = 93;
-const maximum = findMaximum(number1, number2, number3);
-console.log("The maximum between two numbers: " + maximum);
